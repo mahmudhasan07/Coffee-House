ada0605a6e19275e2c28402145b9b307f011edaa
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-02-17 12:18:33 +0600

    Create modal section

--- src/views/UpdateCoffee.vue
+++ src/views/UpdateCoffee.vue
@@ -1,11 +1,17 @@
 <template >
     <section>
-        <div>
-            <h1 class="text-3xl font-semibold text-center my-10">Update OR Delete Your Coffee</h1>
-        </div>
-        <div>
+        <div >
+            <h1 class="text-3xl font-semibold text-center my-10 ">Update OR Delete Your Coffee</h1>
+            <div id="cards" class="flex flex-wrap justify-start gap-10 border-2 w-fit mx-10">
+
+            </div>
             
+
+
+            
+
         </div>
+
     </section>
 </template>
 <script setup>
@@ -15,21 +21,43 @@ import { ref, watchEffect } from 'vue';
 const array = ref("")
 
 
-watchEffect(()=>{
+watchEffect(() => {
     const axiosLink = useAxios(AxiosSecure)
 
-axiosLink.get('/coffees')
-.then(res=>{
-    array.value = res.data
-})
-.catch(error=>{
-console.log(error);
-})
-},[])
+    axiosLink.get('/coffees')
+        .then(res => {
+            array.value = res.data
+            console.log(array.value[0]);
 
-console.log(typeof(array));
+            const cards = document.getElementById('cards')
+            array.value.map((element)=>{
+                const card = document.createElement("div")
+                card.className= "w-80 card p-2 border-2 border-gray-400 rounded-xl"
+                card.innerHTML = `
+                <img class="w-full h-80 object-cover" src="${element.Coffee_image}" alt="">
+            <h1 class="text-2xl font-semibold">${element.Coffee_name}</h1>
+            <p class="text-lg font-semibold">TK. ${element.Coffee_price}</p>
+            <p class='my-2'>${element.Coffee_note}</p>
+
+            <div class="mt-auto flex gap-1">
+                <button class="btn w-1/2">Update</button>
+                <button class="btn w-1/2">Delete</button>
+                </div>
+            
+                `
+                cards.appendChild(card)
+            })
+
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}, [])
 
 </script>
-<style >
-    
-</style>
+<style ></style>
+
+<!-- <div class="border-2 mt-auto flex">
+<button class="btn w-1/2">Update</button>
+<button class="btn w-1/2">Delete</button>
+</div> -->
